6fce1cc0f3436c95a44cd257e857aad8328ff17f
dmytropo <potapchuk50@gmai.com>
2023-08-30 19:23:43 +0200

    style add

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { MovieService } from '../../service/movie.service';
 
+interface Movie {
+  id: number;
+  title: string;
+  overview: string;
+  poster_path: string;
+  showDetails: boolean; // Add this line
+}
+
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css']
 })
 export class MovieListComponent implements OnInit {
-  movies: any[] = [];
+  movies: Movie[] = [];
 
   constructor(private movieService: MovieService) {}
 
   ngOnInit(): void {
     this.movieService.getPopularMovies().subscribe(data => {
-      this.movies = data.results;
+      this.movies = data.results.map((movie: Movie) => ({ ...movie, showDetails: true }));
     });
+  }
+
+  toggleDetails(movie: Movie) {
+    movie.showDetails = !movie.showDetails;
   }
 }
